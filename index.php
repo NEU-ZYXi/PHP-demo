dc2d0f400a587b9bad40178d9c0c58e8ccefd759
NEU-ZYXi <stephenxi@hotmail.com>
2019-02-20 22:07:20 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Xi-Search</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="wrapper index-page">
+		<div class="main-section">
+			<div class="logo">
+				<img src="assets/images/home-logo.png">
+			</div>
+
+			<div class="search-container">
+				<form action="search.php" method="GET">
+					<input type="text" name="term" class="search-box">
+					<input type="submit" value="Search" class="search-button">
+				</form>
+			</div>
+
+			<div class="shortcur">
+				<br><br><br><br><br><br><br><br><br>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,67 @@
+<?php
+
+	if(isset($_GET["term"])) {
+		$term = $_GET["term"];
+	} else {
+		exit("Access Denied");
+	}
+	if ($term == '') {
+		header("Location: http://localhost:8080/Xi-Search/index.php");
+		exit();
+	}
+
+	if(isset($_GET["type"])) {
+		$type = $_GET["type"];
+	} else {
+		$type = "all";
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to Xi-Search</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="wrapper">
+		<div class="header">
+			<div class="header-content">
+				<div class="logo">
+					<a href="index.php">
+						<img src="assets/images/home-logo.png">
+					</a>
+				</div>
+				<div class="search-container">
+					<form action="search.php" method="GET">
+						<div class="search-bar-container">
+							<input type="text" class="search-box" name="term">
+							<button class="search-button">
+								<img src="assets/images/search-icon.png">
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+
+			<div class="tabs-container">
+				<ul class="tabs-list">
+					<li class="<?php echo $type == 'all' ? 'active' : ''; ?>">
+						<a href='<?php echo "search.php?term=$term&type=all"; ?>'>
+							All
+						</a>
+					</li>
+					<li class="<?php echo $type == 'images' ? 'active' : ''; ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
